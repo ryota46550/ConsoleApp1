f0d90774bb4b1afa6fc4a8162124a2170974e9c0
ryota46550 <108723344+ryota46550@users.noreply.github.com>
2022-08-23 16:11:59 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Ex26
+{
+    class Ex26
+    {
+        static void Main(string[] args)
+        {
+            var width = InputUtility.InputFloat("幅");
+            var height = InputUtility.InputFloat("高さ");
+            var depth = InputUtility.InputFloat("奥行");
+            Box box = new Box(width, height, depth);
+            Console.WriteLine($"boxの表面積{box.GetSurface()}boxの体積={box.GetVolume()}");
+
+        }
+
+    }
+}
